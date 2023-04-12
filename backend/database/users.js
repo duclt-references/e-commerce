de4d0104c3a704683be5986d35fbab21eb4f327f
duclt3005 <duclt3005@gmail.com>
2023-04-12 22:07:14 +0700

    feat: fix fake user

--- backend/database/users.js
+++ backend/database/users.js
@@ -1,9 +1,11 @@
 export const users = [
   {
     id: 'jk3z4wo35b0203v',
+    username: 'admin',
     name: 'Admin',
     email: 'admin@gmail.com',
     password: '12345678',
+    passwordConfirm: '12345678',
     gender: 'male',
     phone: '0987654213',
     role: 'admin',
@@ -12,9 +14,11 @@ export const users = [
   },
   {
     id: 's9y0wuc49ehksf5',
+    username: 'duclt',
     name: 'Lai Tien Duc',
     email: 'duclt@gmail.com',
     password: '12345678',
+    passwordConfirm: '12345678',
     gender: 'male',
     phone: '0987654321',
     role: 'user',
